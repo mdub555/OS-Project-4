8c3f4ab659fbc6a602c1b1f0c7ea3b2d1d1625a4
Mitch Waibel <mdub555@gmail.com>
2018-04-19 12:12:32 -0600

    Implemented methods for virtual address

--- src/virtual_address/virtual_address.cpp
+++ src/virtual_address/virtual_address.cpp
@@ -5,23 +5,33 @@
  */
 
 #include "virtual_address/virtual_address.h"
+#include <bitset>
 
 using namespace std;
 
 
 VirtualAddress VirtualAddress::from_string(int process_id, string address) {
-  // TODO: implement me
-  return VirtualAddress(0, 0, 0);
+  // convert the string to a size_t
+  size_t addr = stoi(address, nullptr, 2);
+  // get the page from the addr size_t
+  size_t page = (addr & PAGE_BITMASK) >> OFFSET_BITS;
+  // get the offset from the addr size_t
+  size_t offset = addr & OFFSET_BITMASK;
+  return VirtualAddress(process_id, page, offset);
 }
 
 
 string VirtualAddress::to_string() const {
-  // TODO: implement me
-  return "";
+  // use the feature that does this for me
+  return bitset<PAGE_BITS>(page).to_string() + bitset<OFFSET_BITS>(offset).to_string();
 }
 
 
 ostream& operator <<(ostream& out, const VirtualAddress& address) {
-  // TODO: implement me
+  out << "PID " << address.process_id
+      << " @ " << address.to_string()
+      << " [page: " << address.page
+      << "; offset: " << address.offset
+      << "]";
   return out;
 }
